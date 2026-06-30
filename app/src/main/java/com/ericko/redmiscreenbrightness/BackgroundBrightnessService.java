83eccaba5a03e390876843231dc61c55b2eac078
rabpitvn1-create <rabpitvn1@gmail.com>
2026-06-30 12:59:58 +0700

    Lower dark lux threshold to 15

--- app/src/main/java/com/ericko/redmiscreenbrightness/BackgroundBrightnessService.java
+++ app/src/main/java/com/ericko/redmiscreenbrightness/BackgroundBrightnessService.java
@@ -12,7 +12,7 @@ import android.os.IBinder;
 import android.provider.Settings;
 
 public class BackgroundBrightnessService extends Service implements SensorEventListener {
-    private static final float NORMAL_TO_DARK_LUX = 35.0f;
+    private static final float NORMAL_TO_DARK_LUX = 15.0f;
     private static final float DARK_TO_NORMAL_LUX = 70.0f;
     private static final float NORMAL_TO_BRIGHT_LUX = 800.0f;
     private static final float BRIGHT_TO_NORMAL_LUX = 550.0f;
